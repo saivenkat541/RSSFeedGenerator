cdf926c546a3ec351ea7de8c279e3a702a0f9601
Sai <saivenkat541@outlook.com>
2020-04-06 15:40:55 -0400

    Initial Commit

---
+++ RSSFeedGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSFeedGenerator
+//
+//  Created by Sai Venkat Kancharlapalli on 4/6/20.
+//  Copyright © 2020 Sai Venkat Kancharlapalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
